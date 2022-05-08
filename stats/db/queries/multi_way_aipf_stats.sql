f7b120cec7b43357f0c874402ea95b6718f6b61b
David Havlicek <snex00@gmail.com>
2022-05-08 11:39:13 -0700

    add query for multi-way aipf stats

--- stats/db/queries/multi_way_aipf_stats.sql
+++ stats/db/queries/multi_way_aipf_stats.sql
@@ -1,4 +1,4 @@
--- monthly and yearly stats for pots where we won or lost >= 80bbs
+-- monthly and yearly stats for multi-way aipf pots
 
 select
   num_hands
@@ -7,14 +7,19 @@ select
 from (
   select
     count(hand_histories.id) as num_hands
-    ,sum(case when (result / stakes.stakes_array[1] >= 80) then 1 else 0 end) as pos
-    ,sum(case when (result / stakes.stakes_array[1] <= -80) then 1 else 0 end) as neg
+    ,sum(case when (result / stakes.stakes_array[1] > 0) then 1 else 0 end) as pos
+    ,sum(case when (result / stakes.stakes_array[1] < 0) then 1 else 0 end) as neg
     ,avg(result) as won_per_hand
   from
     hand_histories
     inner join stakes on hand_histories.stake_id = stakes.id
   where
-    (abs(result) / stakes.stakes_array[1]) >= 80
+    all_in = true
+    and flop is null
+    and (
+      note ilike '%side pot%'
+      or note ilike '%vs muck%'
+    )
   ) as foo
 order by 1 desc;
 
@@ -27,14 +32,19 @@ from (
   select
     extract(year from date) as date
     ,count(hand_histories.id) as num_hands
-    ,sum(case when (result / stakes.stakes_array[1] >= 80) then 1 else 0 end) as pos
-    ,sum(case when (result / stakes.stakes_array[1] <= -80) then 1 else 0 end) as neg
+    ,sum(case when (result / stakes.stakes_array[1] > 0) then 1 else 0 end) as pos
+    ,sum(case when (result / stakes.stakes_array[1] < 0) then 1 else 0 end) as neg
     ,avg(result) as won_per_hand
   from
     hand_histories
     inner join stakes on hand_histories.stake_id = stakes.id
   where
-    (abs(result) / stakes.stakes_array[1]) >= 80
+    all_in = true
+    and flop is null
+    and (
+      note ilike '%side pot%'
+      or note ilike '%vs muck%'
+    )
   group by 1
   ) as foo
 order by 1 desc;
@@ -48,14 +58,19 @@ from (
   select
     extract(year from date) || '-' || lpad(extract(month from date)::varchar, 2, '0') as date
     ,count(hand_histories.id) as num_hands
-    ,sum(case when (result / stakes.stakes_array[1] >= 80) then 1 else 0 end) as pos
-    ,sum(case when (result / stakes.stakes_array[1] <= -80) then 1 else 0 end) as neg
+    ,sum(case when (result / stakes.stakes_array[1] > 0) then 1 else 0 end) as pos
+    ,sum(case when (result / stakes.stakes_array[1] < 0) then 1 else 0 end) as neg
     ,avg(result) as won_per_hand
   from
     hand_histories
     inner join stakes on hand_histories.stake_id = stakes.id
   where
-    (abs(result) / stakes.stakes_array[1]) >= 80
+    all_in = true
+    and flop is null
+    and (
+      note ilike '%side pot%'
+      or note ilike '%vs muck%'
+    )
   group by 1
 ) as foo
 order by 1 desc;
