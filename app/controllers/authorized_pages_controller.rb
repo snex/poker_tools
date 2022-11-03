24a12c45162653b6a347ea6c77dd85779ed246cb
David Havlicek <snex00@gmail.com>
2022-11-03 13:12:14 -0700

    remove logger statements

--- app/controllers/authorized_pages_controller.rb
+++ app/controllers/authorized_pages_controller.rb
@@ -1,10 +1,3 @@
 class AuthorizedPagesController < ApplicationController
-  before_action :log_dat_shit
   before_action :require_login
-
-  def log_dat_shit
-    Rails.logger.error("wtf: #{clearance_session.current_user}")
-    Rails.logger.error("wtf: #{session.keys}")
-    Rails.logger.error("wtf: #{cookies.map { |cookie| cookie.join('=') }.join("\n")}")
-  end
 end
