6e0c9cc251da7ec751d61f6621500ac881e833ab
David Havlicek <snex00@gmail.com>
2022-11-24 17:08:26 -0800

    add require line

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@
 require 'mina/rails'
 require 'mina/git'
 require 'mina/rvm' # for rvm support. (https://rvm.io)
+require 'mina/whenever'
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
